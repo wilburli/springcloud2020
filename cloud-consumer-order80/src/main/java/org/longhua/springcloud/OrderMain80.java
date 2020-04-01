4d4756e525a326a40cd70ad6d7fb81f6cfe43d57
wilbur_li <wilbur.li@qq.com>
2020-04-01 22:41:34 +0800

    eureka commit

--- cloud-consumer-order80/src/main/java/org/longhua/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/org/longhua/springcloud/OrderMain80.java
@@ -2,8 +2,11 @@ package org.longhua.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
