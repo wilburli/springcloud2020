fa0e78ea51c833340abf07510fcee25ba57f7db6
wilbur_li <wilbur.li@qq.com>
2020-04-03 23:01:37 +0800

    eureka collect commit

--- cloud-provider-payment8001/src/main/java/org/longhua/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/org/longhua/springcloud/controller/PaymentController.java
@@ -5,7 +5,11 @@ import org.longhua.springcloud.entities.CommonResult;
 import org.longhua.springcloud.entities.Payment;
 import org.longhua.springcloud.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -14,6 +18,9 @@ public class PaymentController {
 
     @Autowired
     private PaymentService paymentService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/insertpayment")
     public CommonResult insertPayment(@RequestBody Payment payment){
@@ -33,6 +40,24 @@ public class PaymentController {
             return CommonResult.ok(payment);
         }
         return CommonResult.ok("无对应数据");
+    }
+
+    @GetMapping("/discovery")
+    public Object getDiscoveryClient(){
+        List<String> servers = discoveryClient.getServices();
+        for (String ele:servers) {
+            log.info("server...{}",ele);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info("instanceId...{}",instance.getInstanceId());
+            log.info("host...{}",instance.getHost());
+            log.info("uri...{}",instance.getUri());
+        }
+
+        return this.discoveryClient;
+
     }
 
 }
