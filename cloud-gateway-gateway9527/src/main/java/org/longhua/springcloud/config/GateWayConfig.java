a7d6b318b0acafc928e9e42e2adcf16525a3e908
wilbur_li <wilbur.li@qq.com>
2020-04-12 21:54:54 +0800

    gateway commit

---
+++ cloud-gateway-gateway9527/src/main/java/org/longhua/springcloud/config/GateWayConfig.java
@@ -0,0 +1,18 @@
+package org.longhua.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        routes.route("path_route_longhua",r->r.path("/guoji").uri("http://news.baidu.com/guoji")).build();
+
+        return routes.build();
+    }
+}
