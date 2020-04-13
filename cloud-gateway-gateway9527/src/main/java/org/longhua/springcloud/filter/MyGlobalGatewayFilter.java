e13f23a8c23d061912954ad9e94e0853d056d2c9
wilbur_li <wilbur.li@qq.com>
2020-04-13 22:52:02 +0800

    gateway filter commit

---
+++ cloud-gateway-gateway9527/src/main/java/org/longhua/springcloud/filter/MyGlobalGatewayFilter.java
@@ -0,0 +1,31 @@
+package org.longhua.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class MyGlobalGatewayFilter implements GlobalFilter,Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (username == null){
+            log.info("用户名不能为空，非法格式");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
---
+++ cloud-gateway-gateway9527/src/test/java/TestUtil.java
@@ -0,0 +1,11 @@
+import java.time.ZonedDateTime;
+
+public class TestUtil {
+
+    public static void main(String[] args) {
+        ZonedDateTime zdt = ZonedDateTime.now();
+        System.out.println(zdt);
+        // 2020-04-13T22:00:56.226+08:00[Asia/Shanghai]
+    }
+
+}
